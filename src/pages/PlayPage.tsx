46b94cbb89f6e1124ef45e13fb2459c2f1e049b0
praneeth003 <ch.krishnapraneeth@gmail.com>
2024-12-17 21:37:42 -0500

    implement play page basics

--- src/pages/PlayPage.tsx
+++ src/pages/PlayPage.tsx
@@ -1,9 +1,27 @@
 import React from "react";
+import { useState } from "react";
 
-export default function PlayPage() {
+const PlayPage: React.FC = () => {
+
+    const [row, setRow] = useState<number>();
+
+    const recordInput = (event: React.ChangeEvent<HTMLInputElement>):void => {
+        setRow(Number(event.target.value));
+    }
+
+    function handleSubmit(event: React.MouseEvent<HTMLButtonElement>): void {
+        event.preventDefault();
+        console.log(row);
+    }
+
   return (
-    <div>
+    <>
       <h1>Play Bingo</h1>
-    </div>
+      <p>Enter a number between 3 to 7</p>
+      <input type = "number" value = {row} placeholder="Enter the number of rows" onChange={recordInput}></input>
+      <button onClick={handleSubmit}>Fill Board</button>
+    </>
   );
 }
+
+export default PlayPage;
