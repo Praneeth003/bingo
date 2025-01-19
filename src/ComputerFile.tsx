68269b2d5958d522e926473dd2338d99236197a3
praneeth003 <ch.krishnapraneeth@gmail.com>
2025-01-19 10:36:42 -0500

    make use of generateBoard function to generate board for computer for playing upon initialization

--- src/ComputerFile.tsx
+++ src/ComputerFile.tsx
@@ -3,21 +3,32 @@ import React from "react";
 import { useEffect } from "react";
 
 interface ComputerFileProps {
-    rows?: number;
+    rows: number;
     markedCells?: string[];
 }
 
 
+const generateBoard = (rows: number): string[] => {
+    const tempArray = [];
+    for (let i = 1; i <= rows * rows; i++){
+        tempArray.push(i.toString());
+    }
+    const shuffledArray = tempArray.sort(() => Math.random() - 0.5);
+    return shuffledArray;
+}
 
 const ComputerFile:React.FC<ComputerFileProps> = ({rows, markedCells}) => {
     const [initialized, setInitialized] = React.useState<boolean>(false);
+    const [computerBoard, setComputerBoard] = React.useState<string[]>([]);
 
     useEffect(() => {
         if (!initialized){
-            console.log("Initialize Computer Player");
+            console.log("Initialized Computer Player");
+            setComputerBoard(generateBoard(rows));
             setInitialized(true);
+            
         }
-    }, [initialized]);
+    }, [initialized, rows]);
 
     useEffect(() => {
         console.log("Computer Plays");
