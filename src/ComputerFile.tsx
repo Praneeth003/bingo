cfb3924991e477a78b236b80e9431482451ca43d
praneeth003 <ch.krishnapraneeth@gmail.com>
2025-01-21 14:25:20 -0500

    change computer file code into a regular code instead of a component

--- src/ComputerFile.tsx
+++ src/ComputerFile.tsx
@@ -1,15 +1,6 @@
-// This file has code to the logic of the computer player
-import React from "react";
-import { useEffect } from "react";
-import Board from "./components/Board";
+// This file contains the common functions that the computer player will use to play the game. The functions are:
 
-interface ComputerFileProps {
-    rows: number;
-    markedCells?: string[];
-}
-
-
-const generateBoard = (rows: number): string[] => {
+export const autoGenereateBoard = (rows: number) => {
     const tempArray = [];
     for (let i = 1; i <= rows * rows; i++){
         tempArray.push(i.toString());
@@ -18,43 +9,3 @@ const generateBoard = (rows: number): string[] => {
     return shuffledArray;
 }
 
-
-
-const ComputerFile:React.FC<ComputerFileProps> = ({rows, markedCells}) => {
-    const [initialized, setInitialized] = React.useState<boolean>(false);
-    const [computerBoard, setComputerBoard] = React.useState<string[]>([]);
-    const [computerMarkedCells, setComputerMarkedCells] = React.useState<string[]>([]);
-
-    // Function to mark the cells as marked 
-    const onMarking = (cellValue: string): void => {
-        setComputerMarkedCells([...computerMarkedCells, cellValue]);
-    }
-
-    useEffect(() => {
-        if (!initialized){
-            console.log("Computer received rows:", rows);
-            console.log("Initialized Computer Player");
-            setComputerBoard(generateBoard(rows));
-            setInitialized(true);       
-        }
-    }, [initialized]);
-
-    // Logging the Computer Board
-    useEffect(() => {
-        console.log("Computer Board is:", computerBoard);  
-    }, [computerBoard]);
-
-
-    // Checking for the changes in the markedCells
-    useEffect(() => {
-        if (initialized){
-            console.log("Computer received markedCells:", markedCells);
-            // Mark the cells in the computer board
-
-        }
-    }, [markedCells]);
-
-    return null;
-}
-
-export default ComputerFile;
--- src/pages/PlayPage.tsx
+++ src/pages/PlayPage.tsx
@@ -2,23 +2,48 @@ import React from "react";
 import { useLocation } from "react-router-dom";
 import { useState } from "react";
 import Board from "../components/Board";
-import ComputerFile from "../ComputerFile";
+import { autoGenereateBoard } from "../ComputerFile";
 
 const PlayPage: React.FC = () => {
     const location = useLocation();
     const cellValues = location.state?.cellValues;
     const rows = location.state?.rows;
     const [markedCells, setMarkedCells] = useState<string[]>([]);
+    const [displayBoard, setDisplayBoard] = useState<boolean>(false);
+
+    let playerPlaysFirst: boolean = true;
+    let isComputerPlayerInitialized: boolean = false;
 
     function onMarking(index: number, cellValue: string){
         console.log("Marking from player", index, cellValue);
         setMarkedCells([...markedCells, cellValue]);
+        
+    }
+
+    function handleToss(event: React.MouseEvent<HTMLButtonElement>): void {
+        event.preventDefault();
+        const toss: boolean = Math.random() > 0.5;
+        if (toss){
+            playerPlaysFirst = true;
+            isComputerPlayerInitialized = true;
+            alert("Player plays first");
+        }
+        else{
+            playerPlaysFirst = false;
+            isComputerPlayerInitialized = true;
+            alert("Computer plays first");
+        }
+        setDisplayBoard(true);
+        // autoGenereateBoard(cellValues, rows, playerPlaysFirst, isComputerPlayerInitialized);
     }
 
     return(
         <>
+        {displayBoard ? (
         <Board cellValues = {cellValues} rows = {rows} onMarking = {onMarking} markedCells = {markedCells}/>
-        <ComputerFile rows = {rows} markedCells = {markedCells}/>
+        ) : (
+            <button onClick={handleToss}>Toss</button>
+        )}  
         </>
     )
 }
